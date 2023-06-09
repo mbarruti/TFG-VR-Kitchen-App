02e86293c3e5504690775bb101c06c84d6e46c24
Manuel Barruti Agapios <mbarruti5@gmail.com>
2023-06-09 19:41:32 +0200

    Added rotation and scale transform cancel. Scale transform in progress.

--- Assets/Scripts/ObjectScripts/BuildingObject.cs
+++ Assets/Scripts/ObjectScripts/BuildingObject.cs
@@ -9,19 +9,11 @@ public class BuildingObject : MonoBehaviour
 
     [SerializeField] Outline outline;
 
-    // Aqui se guarda la transformacion previa a la edicion del objeto seleccionado
+    // TODO: Aqui se guardan las transformaciones previas a la edicion del objeto seleccionado
     //private Transform lastTransform;
     private Vector3 _lastPos;
-
-    // Material original del objeto
-    //private Material material;
-
-    // ---------------------
-
-    //private void OnEnable()
-    //{
-    //    material = meshRenderer.material;
-    //}
+    private Vector3 _lastRot;
+    private Vector3 _lastScale;
 
     // Indica si se puede colocar el objeto o no
     public bool canPlace;
@@ -67,14 +59,14 @@ public class BuildingObject : MonoBehaviour
     public void SavePreviousTransform()
     {
         _lastPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+        _lastRot = new Vector3(gameObject.transform.eulerAngles.x, gameObject.transform.eulerAngles.y, gameObject.transform.eulerAngles.z);
+        _lastScale = new Vector3(gameObject.transform.localScale.x, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
     }
 
     public void SetPreviousTransform()
     {
         gameObject.transform.position = new Vector3(_lastPos.x, _lastPos.y, _lastPos.z);
-        //gameObject.transform.rotation = lastTransform.rotation;
-        //gameObject.transform.localScale = lastTransform.localScale;
-
-        //lastTransform = null;
+        gameObject.transform.eulerAngles = new Vector3(_lastRot.x, _lastRot.y, _lastRot.z);
+        gameObject.transform.localScale = new Vector3(_lastScale.x, _lastScale.y, _lastScale.z);
     }
 }
